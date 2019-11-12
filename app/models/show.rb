8338b217657847daede95a9148218ebbd0826a68
Riley Moore <rilemoore@gmail.com>
2019-11-12 16:40:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,24 @@
 class Show < ActiveRecord::Base
   
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+  
+  def self.most_popular_show
+    
+  end
+  
+  def self.lowest_rating
+    
+  end
+  
+  def self.least_popular_show
+    
+  end
+  
+  def 
+  
+  end
+  
+  
 end
